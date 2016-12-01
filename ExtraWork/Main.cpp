9462d55436f3bd8cc633abf5936a9b9fd456ba86
cjwalle97 <cjwalle97@gmail.com>
2016-12-01 13:53:41 -0600

    Update
    Update containing the Cat Class

--- ExtraWork/Main.cpp
+++ ExtraWork/Main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+//1).
 char Reverse(char Word[])
 {
 	int i = 0;
@@ -14,9 +14,39 @@ char Reverse(char Word[])
 		Word[i - 1 - k] = temp;
 	}
 }
+//2).
+class Cat
+{
+private:
+	char mName;
+	int mAge;
+public:
+	Cat() {};
+	Cat(char a, int b) {
+		mName = a;
+		mAge = b;
+	}
+	void Rename(char other)
+	{
+		mName = other;
+	}
+};
 
+
+
+//4).
+
+//5).
+
+//6).
+
+//7).
+
+//8).
 int main()
 {
 	char Word[12] = "Christopher";
 	char Reverse(Word[12]);
+	//3).
+	
 }
